c0f61315228159004b858d703f3efd3d81ed1fea
jfiedler5 <jos.fiedler@gmail.com>
2023-11-21 21:34:30 +0100

    scraping_core_adjustments
    for optimalization purposes, the code now accesses Seznam's API directly
    
    added requirements.txt

--- listings_scraper/scraper/spiders/sreality_spider.py
+++ listings_scraper/scraper/spiders/sreality_spider.py
@@ -1,63 +1,54 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from ..items import ListingItem
 import scrapy
+import json
+import time
+from ..items import ListingItem
+from scrapy.exceptions import CloseSpider
+
 
 class SrealitySpider(scrapy.Spider):
     name = 'sreality'
     allowed_domains = ['sreality.cz']
-    item_count = 0
-    max_items = 50  # Adjustable number of items
+    item_total = 500
 
     def start_requests(self):
-        url = "https://www.sreality.cz/hledani/prodej/byty?strana=1"
+        """
+        Generates the initial request for the spider.
+        """
+        timestamp = int(time.time() * 1000)
+        url = (f"https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&"
+               f"per_page={self.item_total}&tms={timestamp}")
         yield scrapy.Request(url=url, callback=self.parse_page)
 
     def parse_page(self, response):
-        driver = None
+        """
+        Parses the API response and yields the listings.
+        Closes the spider if the response format is not as expected.
+        """
+        # Attempt to decode JSON
         try:
-            driver = self.init_webdriver(response)
-            listings = self.get_listings(driver)
-            for listing in listings:
-                if self.item_count >= self.max_items:
-                    break
-                yield self.extract_data(listing)
-                self.item_count += 1
-        finally:
-            if driver:
-                driver.quit()
+            data = json.loads(response.text)
+        except json.JSONDecodeError:
+            raise CloseSpider('Invalid JSON response')
 
-        if self.item_count < self.max_items:
-            yield from self.paginate(response)
+        # Check for the expected keys in the JSON data
+        if not all(key in data for key in ('_embedded', 'page')):
+            raise CloseSpider('Unexpected JSON structure')
 
-    def init_webdriver(self, response):
-        """Initialize and return a Selenium WebDriver for the given response."""
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.get(response.url)
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "#page-layout .property.ng-scope"))
-        )
-        return driver
+        # Extract estates data
+        estates = data.get('_embedded', {}).get('estates', [])
+        if not estates:
+            raise CloseSpider('No estates data found')
 
-    def get_listings(self, driver):
-        """Return listings from the page using the given WebDriver."""
-        return scrapy.Selector(text=driver.page_source).css("#page-layout .property.ng-scope")
+        # Process each estate item
+        for index, item in enumerate(estates):
+            yield self.extract_data(item, index)
 
-    def extract_data(self, listing):
-        """Extract and yield data from a single listing."""
-        item_id = f"{self.item_count + 1:04d}"
-        title = listing.css('.text-wrap h2 a.title span.name::text').get()
-        image_url = listing.css('div._15Md1MuBeW62jbm5iL0XqR a:first-child img::attr(src)').get()
+    def extract_data(self, item, index):
+        """
+        Extracts and returns data from a single estate listing item.
+        """
+        item_id = f"{index + 1:04d}"
+        title = item.get('name', 'No Title')
+        image_urls = item.get('_links', {}).get('images', [])
+        image_url = image_urls[0]['href'] if image_urls else 'No Image'
         return ListingItem(id=item_id, title=title, image_url=image_url)
-
-    def paginate(self, response):
-        """Generate the next page request if the item count hasn't reached the maximum."""
-        current_page = response.meta.get('page', 1)
-        next_page = current_page + 1
-        next_page_url = f"https://www.sreality.cz/hledani/prodej/byty?strana={next_page}"
-        return [scrapy.Request(url=next_page_url, callback=self.parse_page, meta={'page': next_page})]
