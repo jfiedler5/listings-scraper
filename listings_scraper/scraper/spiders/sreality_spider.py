75bfbfb533a1db159c691f3ea74144627c9ee4a9
jfiedler5 <jos.fiedler@gmail.com>
2023-11-22 17:12:58 +0100

    spider_user_agent
    Added spider user agent for matching the user's data (fixes the difference in returned meters squared value for a user and a scraper)

--- listings_scraper/scraper/spiders/sreality_spider.py
+++ listings_scraper/scraper/spiders/sreality_spider.py
@@ -7,6 +7,9 @@ from scrapy.exceptions import CloseSpider
 
 class SrealitySpider(scrapy.Spider):
     name = 'sreality'
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
     allowed_domains = ['sreality.cz']
     item_total = 500
 
